356e60f9c07d56f96e18effed6f529e8850e1988
NadetDev <nadetdev@gmail.com>
2025-01-06 17:07:14 +0100

    add unit tests for compoment + service

--- src/app/components/todo/todo.component.spec.ts
+++ src/app/components/todo/todo.component.spec.ts
@@ -1,23 +1,82 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import { TodoService } from '../../services/todo.service';
+import { of } from 'rxjs';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let todoService: jasmine.SpyObj<TodoService>;
 
   beforeEach(async () => {
+    const todoServiceSpy = jasmine.createSpyObj('TodoService', [
+      'getAllTodos',
+      'addTodo',
+      'updateTodo',
+      'deleteTodo',
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [TodoComponent]
-    })
-    .compileComponents();
+      imports: [TodoComponent],
+      providers: [{ provide: TodoService, useValue: todoServiceSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    todoService = TestBed.inject(TodoService) as jasmine.SpyObj<TodoService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load todos on init', () => {
+    const mockTodos = [{ id: 1, title: 'Test Todo', completed: false }];
+    todoService.getAllTodos.and.returnValue(mockTodos);
+
+    fixture.detectChanges();
+
+    expect(todoService.getAllTodos).toHaveBeenCalled();
+    expect(component.todos).toEqual(mockTodos);
+  });
+
+  it('should add a todo', () => {
+    component.newTodoTitle = 'New Todo';
+    todoService.addTodo.and.callFake((title: string) => {
+      const newTodo = { id: 1, title, completed: false };
+      todoService.getAllTodos.and.returnValue([newTodo]);
+      return newTodo;
+    });
+
+    component.addTodo();
+
+    expect(todoService.addTodo).toHaveBeenCalledWith('New Todo');
+    expect(component.newTodoTitle).toBe('');
+  });
+
+  it('should not add empty todo', () => {
+    component.newTodoTitle = '   ';
+    component.addTodo();
+
+    expect(todoService.addTodo).not.toHaveBeenCalled();
+  });
+
+  it('should toggle todo completion', () => {
+    const mockTodo = { id: 1, title: 'Test Todo', completed: false };
+    todoService.updateTodo.and.returnValue(undefined);
+
+    component.toggleComplete(mockTodo);
+
+    expect(todoService.updateTodo).toHaveBeenCalledWith(1, { completed: true });
+  });
+
+  it('should delete a todo', () => {
+    const mockTodoId = 1;
+    todoService.deleteTodo.and.returnValue(undefined);
+
+    component.deleteTodo(mockTodoId);
+
+    expect(todoService.deleteTodo).toHaveBeenCalledWith(1);
   });
 });
--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
@@ -12,5 +11,41 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a todo', () => {
+    const initialCount = service.getAllTodos().length;
+    service.addTodo('Test Todo');
+    expect(service.getAllTodos().length).toBe(initialCount + 1);
+  });
+
+  it('should get all todos', () => {
+    service.addTodo('Test Todo 1');
+    service.addTodo('Test Todo 2');
+    const todos = service.getAllTodos();
+    expect(todos.length).toBe(2);
+  });
+
+  it('should update a todo', () => {
+    service.addTodo('Test Todo');
+    const todo = service.getAllTodos()[0];
+    service.updateTodo(todo.id, { completed: true });
+    const updatedTodo = service.getAllTodos()[0];
+    expect(updatedTodo.completed).toBeTrue();
+  });
+
+  it('should delete a todo', () => {
+    service.addTodo('Test Todo');
+    const initialCount = service.getAllTodos().length;
+    const todo = service.getAllTodos()[0];
+    service.deleteTodo(todo.id);
+    expect(service.getAllTodos().length).toBe(initialCount - 1);
+  });
+
+  it('should generate unique ids', () => {
+    service.addTodo('Test Todo 1');
+    service.addTodo('Test Todo 2');
+    const todos = service.getAllTodos();
+    expect(todos[0].id).not.toBe(todos[1].id);
   });
 });
